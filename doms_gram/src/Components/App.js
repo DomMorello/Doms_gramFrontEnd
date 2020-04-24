a804e246904c2aec66754aac11fcde9446061464
donglee <donglee@student.42seoul.kr>
2020-04-24 21:42:51 +0900

    login front-end

--- doms_gram/src/Components/App.js
+++ doms_gram/src/Components/App.js
@@ -1,9 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
-class App extends Component {
-  render() {
-    return <div className="App" />;
+const QUERY = gql`
+  {
+    isLoggedIn @client
   }
-}
+`;
 
-export default App;
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn },
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
